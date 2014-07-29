68e93aa22aac4cb0bbfd22e0b3076bc4e64895ad
root <root@ceph1.(none)>
2014-07-29 17:35:33 +0800

    improve the html output

--- ceph/ceph_daily_workload_report.py
+++ ceph/ceph_daily_workload_report.py
@@ -9,40 +9,79 @@ import time
 
 def check_ceph_daily_workload(dict1):
 	dict1=check_health(dict1)
+	dict1=check_pool(dict1)
 	return dict1
 
 def check_health(dict1):
 	print "<!--Checking ceph health-->"
-     	cmd="ceph health"
+     	cmd="ceph status"
      	dict1['cmd_ceph_health']=cmd
      	ret,output = commands.getstatusoutput(cmd)
      	output=output.replace('\n','<br>')
-	dict1["ceph_health"]=output
+	dict1["ceph health"]=output
 	return dict1
 
-def check_rados(dict1):
-
+def check_pool(dict1):
+	print "<!--Checking ceph pool status-->"
+     	cmd="ceph osd dump|grep pool"
+     	dict1['cmd_ceph_pool_status']=cmd
+     	ret,output = commands.getstatusoutput(cmd)
+     	output=output.replace('\n','<br>')
+	dict1["ceph pool status"]=output
 	return dict1
 
 def get_html_body(dict1):
 	date=time.strftime("%Y/%m/%d %H:%M:%S")
 	hostname=socket.gethostname()
+	list=['ceph health','ceph pool status']
 	msg_head='''<html>
 <head>
 <title></title>
+<style type="text/css">
+table { 
+	background:#F00
+	border-width:2;
+        border-style: solid;
+} 
+
+th {
+        border-width: 1px;
+        padding: 8px;
+        border-style: solid;
+        border-color: #666666;
+        background-color: #00FF00;
+	text-align: left;
+	font-size:20;
+}
+
+td {
+	font-size:15px;
+	text-align: left;
+}
+
+</style>
 </head><body>'''
-	msg="<H1>Ceph Daily Workload Report</H1>"
+	msg="<H2>Ceph Daily Workload Report</H2>"
 	msg=msg+"Hostname: "+hostname+"<br>"
-	msg=msg+"Date: "+date+"<br>"
+	msg=msg+"Date: "+date+"<br><br>"
 
+	#msg=msg+'<table class="gridtable"><tr class="heading">'
+	msg=msg+'<table >\n'
 	
-	list=['ceph_health']
-	for key in list:
-		msg=msg+"<b>"+key+"</b><br>"
-		msg=msg+dict1[key]+"<br>"
+	#for key in list:
+	#	if dict1.has_key(key):
+	#		msg=msg+"<b>"+key+"</b><br>"
+	#		msg=msg+dict1[key]+"<br><br>"
 
+	for key in list:
+		if dict1.has_key(key):
+			msg=msg+"<tr><th>"+key+"</th></tr>\n"+'<tr><td>'
+			msg=msg+dict1[key]+"</td></tr>\n"
+
+	msg=msg+'</tr></table>'
 	msg_end='''</body></html>'''
 	msg=msg_head+msg+msg_end
+	print msg
 	return msg
 
 def output_report(dict1):
