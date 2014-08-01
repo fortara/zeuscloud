bcabae7ba6a1c936134693956675c58deb621738
root <root@ceph1.(none)>
2014-08-01 16:00:42 +0800

    update ceph daily report script

--- ceph/ceph_daily_workload_report.py
+++ ceph/ceph_daily_workload_report.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #2014/7/28 this script will generate ceph daily workload report
 import sys
+import re
 import socket
 import commands
 import smtplib
@@ -9,6 +10,8 @@ import time
 
 def check_ceph_daily_workload(dict1):
 	dict1=check_health(dict1)
+	dict1=check_df(dict1)
+	dict1=check_rados_df(dict1)
 	dict1=check_pool(dict1)
 	return dict1
 
@@ -18,7 +21,26 @@ def check_health(dict1):
      	dict1['cmd_ceph_health']=cmd
      	ret,output = commands.getstatusoutput(cmd)
      	output=output.replace('\n','<br>')
+     	output=output.replace(' ','&nbsp;')
 	dict1["ceph health"]=output
+	return dict1
+
+def check_df(dict1):
+	print "<!--Checking ceph df-->"
+     	cmd="ceph df"
+     	ret,output = commands.getstatusoutput(cmd)
+     	output=output.replace('\n','<br>\n')
+     	output=output.replace(' ','&nbsp;')
+	dict1["ceph df"]=output
+	return dict1
+
+def check_rados_df(dict1):
+	print "<!--Checking rados df-->"
+     	cmd="rados df"
+     	ret,output = commands.getstatusoutput(cmd)
+     	output=output.replace('\n','<br>\n')
+     	output=output.replace(' ','&nbsp;')
+	dict1["rados df"]=output
 	return dict1
 
 def check_pool(dict1):
@@ -27,6 +49,7 @@ def check_pool(dict1):
      	dict1['cmd_ceph_pool_status']=cmd
      	ret,output = commands.getstatusoutput(cmd)
      	output=output.replace('\n','<br>')
+     	output=output.replace(' ','&nbsp;')
 	dict1["ceph pool status"]=output
 	return dict1
 
@@ -40,7 +63,7 @@ def run_command(dict1,key,command):
 def get_html_body(dict1):
 	date=time.strftime("%Y/%m/%d %H:%M:%S")
 	hostname=socket.gethostname()
-	list=['ceph health','ceph pool status']
+	list=['ceph health','ceph df','rados df','ceph pool status']
 	msg_head='''<html>
 <head>
 <title></title>
@@ -84,11 +107,14 @@ td {
 		if dict1.has_key(key):
 			msg=msg+"<tr><th>"+key+"</th></tr>\n"+'<tr><td>'
 			msg=msg+dict1[key]+"</td></tr>\n"
+			#print "--------------------------------------"
+			#print dict1[key]
+			#print "--------------------------------------"
 
 	msg=msg+'</tr></table>'
 	msg_end='''</body></html>'''
 	msg=msg_head+msg+msg_end
-	print msg
+	#print msg
 	return msg
 
 def output_report(dict1):
@@ -96,11 +122,24 @@ def output_report(dict1):
 	hostname=socket.gethostname()
 
 	sender = 'ceph-user@'+hostname
-	receiver = 'andrew_yu@zeuscloud.cn'  
+	#receiver = 'andrew_yu@zeuscloud.cn;sean_cao@zeuscloud.cn'
+	#receiver = 'sean_cao@zeuscloud.cn;;andrew_yu@zeuscloud.cn'
+	receiver = ['sean_cao@zeuscloud.cn','andrew_yu@zeuscloud.cn','netlink_xiao@zeuscloud.cn']
 	subject = 'Ceph DWR '+date
 	smtpserver = 'localhost'  
 	username = 'root'  
 	password = 'welcome1'  
+
+	#pattern = re.compile(r'health HEALTH_(\w+) ')
+	#pattern = re.compile(r'health&nbsp;HEALTH_(\w+)&nbsp;')
+	pattern=re.compile(r'HEALTH_(\w+)')
+	#print dict1['ceph health']
+	match = pattern.search(dict1['ceph health'])
+	if match:
+		ceph_status=match.groups()[0]
+	else:
+		ceph_status="unmatched"
+	subject=subject+" ["+ceph_status+"]"
 	  
 	#msg = MIMEText('<html><h1>hello world</h1></html>','html','utf-8')  
 	msgbody=get_html_body(dict1)
@@ -108,6 +147,8 @@ def output_report(dict1):
 	msg=MIMEText(msgbody,'html','utf-8')
 	  
 	msg['Subject'] = subject  
+	msg['to']=";".join(receiver)
+	print msg['to']
 	  
 	smtp = smtplib.SMTP()  
 	smtp.connect('localhost')  
